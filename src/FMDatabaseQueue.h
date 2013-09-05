719926f26fc62538e00ceff461d1a8d5ef570054
Cliff Smyrl <smyrl@digitalcyclone.com>
2013-09-05 14:22:20 -0500

    Stopped using dispatch_sync in favor of NSRecursiveLock in order to maintain the current behavior minus the deadlock issues associated with dispatch_sync.

--- src/FMDatabaseQueue.h
+++ src/FMDatabaseQueue.h
@@ -13,7 +13,6 @@
 
 @interface FMDatabaseQueue : NSObject {
     NSString            *_path;
-    dispatch_queue_t    _queue;
     FMDatabase          *_db;
 }
 
@@ -29,8 +28,6 @@
 - (void)inDeferredTransaction:(void (^)(FMDatabase *db, BOOL *rollback))block;
 
 #if SQLITE_VERSION_NUMBER >= 3007000
-// NOTE: you can not nest these, since calling it will pull another database out of the pool and you'll get a deadlock.
-// If you need to nest, use FMDatabase's startSavePointWithName:error: instead.
 - (NSError*)inSavePoint:(void (^)(FMDatabase *db, BOOL *rollback))block;
 #endif
 
--- src/FMDatabaseQueue.m
+++ src/FMDatabaseQueue.m
@@ -12,36 +12,24 @@
 #define THREAD_ID_KEY @"FMDB_THREAD_ID"
 
 @interface FMDatabaseQueue ()
-@property (nonatomic, strong) NSString *queueUUID;
+@property (nonatomic, strong) NSRecursiveLock *lock;
 @end
-
-/*
- 
- Note: we call [self retain]; before using dispatch_sync, just incase
- FMDatabaseQueue is released on another thread and we're in the middle of doing
- something in dispatch_sync
- 
- */
 
 @implementation FMDatabaseQueue
 
 @synthesize path = _path;
 
-+ (id)databaseQueueWithPath:(NSString*)aPath {
-    
++ (id)databaseQueueWithPath:(NSString*)aPath
+{
     FMDatabaseQueue *q = [[self alloc] initWithPath:aPath];
-    
     FMDBAutorelease(q);
-    
     return q;
 }
 
-- (id)initWithPath:(NSString*)aPath {
-    
+- (id)initWithPath:(NSString*)aPath
+{
     self = [super init];
-    
     if (self != nil) {
-        
         _db = [FMDatabase databaseWithPath:aPath];
         FMDBRetain(_db);
         
@@ -52,39 +40,33 @@
         }
         
         _path = FMDBReturnRetained(aPath);
-        
-        _queue = dispatch_queue_create([[NSString stringWithFormat:@"fmdb.%@", self] UTF8String], NULL);
-        _queueUUID = FMDBReturnRetained([FMDatabaseQueue generateUniqueIdentifier]);
+        _lock = [[NSRecursiveLock alloc] init];
     }
     
     return self;
 }
 
-- (void)dealloc {
-    
+- (void)dealloc
+{
     FMDBRelease(_db);
     FMDBRelease(_path);
-    FMDBRelease(_queueUUID);
-    if (_queue) {
-        FMDBDispatchQueueRelease(_queue);
-        _queue = 0x00;
-    }
+    FMDBRelease(_lock);
 #if ! __has_feature(objc_arc)
     [super dealloc];
 #endif
 }
 
-- (void)close {
-    FMDBRetain(self);
-    dispatch_sync(_queue, ^() {
-        [_db close];
-        FMDBRelease(_db);
-        _db = 0x00;
-    });
-    FMDBRelease(self);
+- (void)close
+{
+    [_lock lock];
+    [_db close];
+    FMDBRelease(_db);
+    _db = 0x00;
+    [_lock unlock];
 }
 
-- (FMDatabase*)database {
+- (FMDatabase*)database
+{
     if (!_db) {
         _db = FMDBReturnRetained([FMDatabase databaseWithPath:_path]);
         
@@ -99,57 +81,23 @@
     return _db;
 }
 
-- (void)inDatabase:(void (^)(FMDatabase *db))block {
-    FMDBRetain(self);
-    // Check for buggy reentrant code
-    NSString *threadId = [[[NSThread currentThread] threadDictionary] objectForKey:THREAD_ID_KEY];
-    if ([_queueUUID isEqualToString:threadId])
-    {
-#ifdef DEBUG
-        NSAssert(NO, @"FMDB dispatch_sync called while already on the target queue!");
-#endif
-        FMDatabase *db = [self database];
-        block(db);
-        [db closeOpenResultSets];
-    }
-    else {
-        dispatch_sync(_queue, ^() {
-            [[[NSThread currentThread] threadDictionary] setObject:_queueUUID forKey:THREAD_ID_KEY];
-            FMDatabase *db = [self database];
-            block(db);
-            [db closeOpenResultSets];
-            [[[NSThread currentThread] threadDictionary] removeObjectForKey:THREAD_ID_KEY];
-        });
-    }
-    
-    FMDBRelease(self);
+- (void)inDatabase:(void (^)(FMDatabase *db))block
+{
+    [_lock lock];
+    FMDatabase *db = [self database];
+    block(db);
+    [db closeOpenResultSets];
+    [_lock unlock];
 }
 
 
-- (void)beginTransaction:(BOOL)useDeferred withBlock:(void (^)(FMDatabase *db, BOOL *rollback))block {
-    FMDBRetain(self);
-    // Check for buggy reentrant code
-    NSString *threadId = [[[NSThread currentThread] threadDictionary] objectForKey:THREAD_ID_KEY];
-    if ([_queueUUID isEqualToString:threadId])
-    {
-#ifdef DEBUG
-        NSAssert(NO, @"FMDB dispatch_sync called while already on the target queue!");
-#endif
-        FMDatabase *db = [self database];
-        [self performTransactionBlock:block withDatabase:db useDeferred:useDeferred];
-        [db closeOpenResultSets];
-    }
-    else {
-        dispatch_sync(_queue, ^() {
-            [[[NSThread currentThread] threadDictionary] setObject:_queueUUID forKey:THREAD_ID_KEY];
-            FMDatabase *db = [self database];
-            [self performTransactionBlock:block withDatabase:db useDeferred:useDeferred];
-            [db closeOpenResultSets];
-            [[[NSThread currentThread] threadDictionary] removeObjectForKey:THREAD_ID_KEY];
-        });
-    }
-    
-    FMDBRelease(self);
+- (void)beginTransaction:(BOOL)useDeferred withBlock:(void (^)(FMDatabase *db, BOOL *rollback))block
+{
+    [_lock lock];
+    FMDatabase *db = [self database];
+    [self performTransactionBlock:block withDatabase:db useDeferred:useDeferred];
+    [db closeOpenResultSets];
+    [_lock unlock];
 }
 
 - (void)performTransactionBlock:(void (^)(FMDatabase *db, BOOL *rollback))block withDatabase:(FMDatabase *)db useDeferred:(BOOL)useDeferred
@@ -174,58 +122,41 @@
 
 }
 
-- (void)inDeferredTransaction:(void (^)(FMDatabase *db, BOOL *rollback))block {
+- (void)inDeferredTransaction:(void (^)(FMDatabase *db, BOOL *rollback))block
+{
     [self beginTransaction:YES withBlock:block];
 }
 
-- (void)inTransaction:(void (^)(FMDatabase *db, BOOL *rollback))block {
+- (void)inTransaction:(void (^)(FMDatabase *db, BOOL *rollback))block
+{
     [self beginTransaction:NO withBlock:block];
 }
 
 #if SQLITE_VERSION_NUMBER >= 3007000
-- (NSError*)inSavePoint:(void (^)(FMDatabase *db, BOOL *rollback))block {
-    
+- (NSError*)inSavePoint:(void (^)(FMDatabase *db, BOOL *rollback))block
+{
+    NSError *err = 0x00;
+    [_lock lock];
     static unsigned long savePointIdx = 0;
-    __block NSError *err = 0x00;
-    FMDBRetain(self);
-    dispatch_sync(_queue, ^() {
-        
-        NSString *name = [NSString stringWithFormat:@"savePoint%ld", savePointIdx++];
-        
+    NSString *name = [NSString stringWithFormat:@"savePoint%ld", savePointIdx++];
+    FMDatabase *db = [self database];
+
+    if ([db startSavePointWithName:name error:&err]) {
         BOOL shouldRollback = NO;
-        
-        FMDatabase *db = [self database];
-        if ([db startSavePointWithName:name error:&err]) {
-            
-            block(db, &shouldRollback);
-            [db closeOpenResultSets];
-            
-            if (shouldRollback) {
-                [db rollbackToSavePointWithName:name error:&err];
-            }
-            else {
-                [db releaseSavePointWithName:name error:&err];
-            }
-            
+        block(db, &shouldRollback);
+        [db closeOpenResultSets];
+
+        if (shouldRollback) {
+            [db rollbackToSavePointWithName:name error:&err];
         }
-    });
-    FMDBRelease(self);
+        else {
+            [db releaseSavePointWithName:name error:&err];
+        }
+    }
+
+    [_lock unlock];
     return err;
 }
 #endif
 
-+ (NSString *)generateUniqueIdentifier
-{
-    // Make the compiler happy, we only use this under ARC anyways.
-    NSString *uuid = nil;
-    CFUUIDRef uuidCreator = CFUUIDCreate(kCFAllocatorDefault);
-    CFStringRef uuidStringRef = CFUUIDCreateString(kCFAllocatorDefault, uuidCreator);
-    CFRelease(uuidCreator);
-#if __has_feature(objc_arc)
-    uuid = (__bridge_transfer NSString *)uuidStringRef;
-#else
-    uuid = [(NSString *)uuidStringRef autorelease];
-#endif
-    return uuid;
-}
 @end
